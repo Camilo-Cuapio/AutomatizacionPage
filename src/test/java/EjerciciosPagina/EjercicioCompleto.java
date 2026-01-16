d4b747b3444c4690bfacadf222a2f5e35776cde9
Camilo-Cuapio <cuapio.camilo@gmail.com>
2026-01-16 14:27:01 -0600

    Se agrega logeo

--- src/test/java/EjerciciosPagina/EjercicioCompleto.java
+++ src/test/java/EjerciciosPagina/EjercicioCompleto.java
@@ -16,5 +16,32 @@ import java.util.Collections;
 import java.util.List;
 
 public class EjercicioCompleto {
-    private
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+    }
+    @Test
+    public void logeo(){
+     WebElement usu= driver.findElement(By.xpath("//*[@id=\"user-name\"]"));
+     usu.click();
+     usu.sendKeys("standard_user");
+     WebElement pass= driver.findElement(By.xpath("//*[@name=\"password\"]"));
+     pass.click();
+     pass.sendKeys("secret_sauce");
+pass.submit();
+
+String validarLog= driver.getCurrentUrl();
+        System.out.println("la url es: "+validarLog);
+        if (validarLog.contains("inventory.html")){
+            System.out.println("logeo correcto");
+        }else {
+            System.out.println("logeo incorrecto");
+        }
+
+    }
 }
